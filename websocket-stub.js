4ab0948c7a1b7b4701ea42028d93df830d449879
echos.git <ephshal@gmail.com>
2021-08-26 21:19:17 -0700

    #3 partial

--- websocket-stub.js
+++ websocket-stub.js
@@ -1,14 +1,12 @@
-var ws = new WebSocket("ws://3.144.116.72/api/run");
+var ws = new WebSocket("ws://135.181.63.205/api/run");
 
 var obj = {prompts: ["your prompt here"]};
 var msg = msgpack.serialize(obj);
 ws.send(msg);
+console.log("buzz");
 
 ws.addEventListener('message', function (event) {
-  var obj = msgpack.deserialize(event.data);
-  var png_bytes = new Blob(obj["img"], {type: "image/png"});
-  var iter = obj["iter"];
-  var score = obj["score"];
+  var png_bytes = new Blob(event.data, {type: "image/png"});
   // TODO: whatever
   img_link.href = URL.createObjectUrl(png_bytes)
 });
